49a2ef6ad611038036adb8e013f17c84d13b27b9
zzinLee <sujin951017@gmail.com>
2024-05-02 13:12:36 +0900

    React | memoized Component | add description

--- src/bodyRoot/Article.jsx
+++ src/bodyRoot/Article.jsx
@@ -16,6 +16,8 @@ function Article({ number }) {
   );
 }
 
+//아래의 주석 2줄을 해제 하면 memo된 Article 을 사용해 볼 수 있습니다. 이때 export default Artilce; 은 주석 처리해주세요.
 // const MemoArticle = memo(Article);
 // export default MemoArticle;
+
 export default Article;
