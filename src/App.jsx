9bbd10a3e238a6d5cdde4ee11bd7a350cd108d5c
zzinLee <sujin951017@gmail.com>
2024-05-21 13:39:02 +0900

    Avoid Duplication in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import { useState } from "react";
-import Message from "./Message";
+
+const initialItems = [
+  { title: "pretzels", id: 0 },
+  { title: "crispy seaweed", id: 1 },
+  { title: "granola bar", id: 2 },
+];
 
 function App() {
-  const [messageColor, setMessageColor] = useState("blue");
-  const changeColor = () => {
-    setMessageColor("yellow");
+  const [items, setItems] = useState(initialItems);
+  const [selectedItemId, setSelectedItemId] = useState(0);
+  const handleChangeInput = (ev, id) => {
+    const changedItems = items.map((item) => {
+      return (item.id === id) ? {
+          ...item,
+          title: ev.target.value,
+        } : item;
+    });
+
+    setItems(changedItems);
   };
+  const handleChooseButton = (ev, id) => {
+    ev.stopPropagation();
+
+    setSelectedItemId(id);
+  };
+
+  const itemElemList = items.map((item) => {
+    return (
+      <li key={item.id}>
+        <input type="text" value={item.title} onChange={(ev) => handleChangeInput(ev, item.id)} />
+        <button onClick={(ev) => handleChooseButton(ev, item.id)}>choose</button>
+      </li>
+    );
+  });
+
+  const selectedItem = items.find((item) => item.id === selectedItemId);
 
   return (
     <>
-      <button onClick={changeColor}>messageColor변경</button>
-      <Message messageColor={messageColor} />
+      <h3>{"What's your travel snack?"}</h3>
+      {itemElemList}
+      <p>You picked {selectedItem.title}</p>
     </>
   );
 }
