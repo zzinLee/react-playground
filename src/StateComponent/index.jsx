8e5fcb091fb9cecb32c6588f1371ab62eac11288
zzinLee <sujin951017@gmail.com>
2024-06-06 07:44:48 +0900

    Use Reducer with Context : Initial Commit

--- src/StateComponent/index.jsx
+++ src/StateComponent/index.jsx
@@ -1,37 +1,72 @@
-//1. createContext를 import 합니다.
-import { useState, createContext } from "react";
+import { useState, useReducer } from "react";
+import { TasksContext, TasksDispatchContext } from "../context/tasksContext";
 
-import LocalOne from "../LocalOne";
-import LocalTwo from "../LocalTwo";
+import TaskList from "../TaskList";
 
-import { diceResult } from "../utils";
+const initialTasks = [
+  { id: 0, text: "Philosopher’s Path", done: true },
+  { id: 1, text: "Visit the temple", done: false },
+  { id: 2, text: "Drink matcha", done: false },
+];
 
-//2. 컴포넌트 외부에 최초전달할 값을 인자로 삼는 createContext를 호출하고, 컨텍스트를 생성합니다.
-export const DiceContext = createContext("최초값");
+const tasksReducer = (tasks, action) => {
+  switch (action.type) {
+    case "add": {
+      return [...tasks, {
+        id: action.id,
+        text: action.text,
+        done: false,
+      }];
+    }
+
+    case "change": {
+      return tasks.map((task) => {
+        if (task.id === action.task.id) {
+          return action.task;
+        } else {
+          return task;
+        }
+      });
+    }
+
+    case "delete": {
+      return tasks.filter((task) => task.id !== action.id);
+    }
+
+    default: {
+      throw Error("Unknown Action: " + action.type);
+    }
+  }
+}
 
 function StateComponent() {
-  const initaialDiceResult = diceResult();
-  //전달하고자 하는 상태는 그대로 정의합니다.
-  const [dice, setDice] = useState(initaialDiceResult);
+  console.log("✨ StateComponent App render");
+  const [tasks, dispatch] = useReducer(
+    tasksReducer, initialTasks
+  );
 
-  const replay = (ev) => { //re-rendering 을 촉진시키는 함수.
-    ev.preventDefault();
-
-    const resettedDice = diceResult();
-
-    setDice(resettedDice);
+  const handleChangeTask = (task) => {
+    dispatch({
+      type: "change",
+      task: task,
+    });
+  };
+  const handleDeleteTask = (taskId) => {
+    dispatch({
+      type: "delete",
+      id: taskId,
+    });
   };
 
   return (
-    //3. DiceContext.Provider로 감싸고 value 값에 원하는 상태값을 전달합니다.
-    //하위컴포넌트가 어디에 있어도 Provider가 value를 제공합니다.
-    <DiceContext.Provider value={dice}>
-      <LocalOne />
-      <div>
-        <LocalTwo />
-        <button onClick={replay}>replay</button>
-      </div>
-    </DiceContext.Provider>
+    <>
+      <h1>Day off in Kyoto</h1>
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
   );
 }
 
